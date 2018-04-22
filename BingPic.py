23c91ba4e3cb66e963f12d24f6a9457b5b155344
ArtUsr <Avkiki@live.cn>
2018-04-22 11:23:06 +0800

    add downloader

--- BingPic.py
+++ BingPic.py
@@ -1,10 +1,20 @@
 import requests
-
+import os
 head = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36'
 }
 
-response = requests.get(url="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=5&n=10&nc=1524303558367&pid=hp",headers=head)
-hjson = response.json()
-get_url = 'https://cn.bing.com' + hjson['images'][7]['url']
-print(get_url)
+def get_img_url():
+    for idx in range(8):
+        response = requests.get(url="https://cn.bing.com/HPImageArchive.aspx?format=js&n=10&idx="+str(idx),headers=head)
+        hjson = response.json()
+        get_url = 'https://cn.bing.com' + hjson['images'][0]['url']
+        get_title = hjson['images'][0]['enddate']
+        img_date = requests.get(get_url,headers=head).content
+        with open('%s.jpg'%get_title,'wb') as f:
+            f.write(img_date)
+            print('正在下载',get_title)
+
+
+if __name__ == '__main__':
+    get_img_url()
